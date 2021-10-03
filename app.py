5d831db85bdcb9fbc47928b84ec50d759618f2bd
mcodemax <mwalterjohnson7@gmail.com>
2021-10-03 00:21:59 -0700

    debugging2

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import requests
 
 CURR_USER_KEY = "curr_user"
 close_price_dict = {}
-print(ALPHA_VAN_API_KEY)
+print(SECRET_KEY)
 
 app = Flask(__name__)
 CORS(app) #https://flask-cors.readthedocs.io/en/latest/
